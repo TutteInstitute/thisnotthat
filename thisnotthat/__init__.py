ddf20c4f2c2e7a71727fbee5b4c1402caaffb864
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2021-11-29 11:48:08 -0500

    Editable legend that merges clusters

--- thisnotthat/__init__.py
+++ thisnotthat/__init__.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+import itertools as it
 from typing import *
 
 import bqplot as bq
@@ -24,8 +25,9 @@ class Labeler(wg.HBox):
         labels: Sequence[Hashable],
         notes: Sequence[str] = [],
         names: Mapping[Hashable, Any] = {},
+        colors: Mapping[Hashable, str] = {}
     ) -> None:
-        super().__init__(children=[], layout=wg.Layout(width="800px", height="800px", resize="both"))
+        super().__init__(children=[], layout=wg.Layout(width="800px", height="600px", resize="both"))
         assert data.shape[0] == len(labels)
         assert data.shape[1] == 2
         self.data = data
@@ -33,52 +35,114 @@ class Labeler(wg.HBox):
         self.notes = [*notes]
 
         self.names = {**names}
+        self.colors = {**colors}
         index_unknown = 0
-        for label in uniques_sorted(self.labels):
+        for label, color in zip(uniques_sorted(self.labels), it.cycle(COLORS)):
             if label not in self.names:
                 index_unknown += 1
                 self.names[label] = f"Cluster {index_unknown}"
+            if label not in self.colors:
+                self.colors[label] = color
 
         self.refresh()
 
     def refresh(self) -> None:
+        self.plot = self._make_plot()
+        self.children = [self.plot, self._make_legend()]
+
+    def _make_plot(self) -> bq.Figure:
         scale_x = bq.LinearScale()
         scale_y = bq.LinearScale()
         scatters = []
-        for label, color in zip(uniques_sorted(self.labels), COLORS):
+        for label in uniques_sorted(self.labels):
             i_labeled = [i for i, l_ in enumerate(self.labels) if l_ == label]
             data_labeled = self.data[i_labeled]
             if self.notes:
                 raise NotImplementedError("Tooltips")
             scatters.append(
                 bq.Scatter(
+                    name=label,
                     x=data_labeled[:, 0],
                     y=data_labeled[:, 1],
-                    colors=[color],
+                    colors=[self.colors[label]],
                     scales={"x": scale_x, "y": scale_y},
-                    names=[self.names[self.labels[i]] for i in i_labeled],
                     display_names=False
                 )
             )
 
-        self.figure = bq.Figure(
+        return bq.Figure(
             marks=scatters,
             axes=[
                 bq.Axis(scale=scale_x),
                 bq.Axis(scale=scale_y, orientation="vertical")
             ],
-            layout=wg.Layout(width="95%", height="95%"),
+            layout=wg.Layout(height="98%"),
             fig_margin={"top": 15, "bottom": 15, "left": 15, "right": 15},
             min_aspect_ratio=1.0,
             max_aspect_ratio=1.0
         )
-        self.children = [self.figure]
 
+    def _update_name(self, label: Hashable) -> None:
+        def _update(change: Dict):
+            name_new = change["new"]
+            label_merge = None
+            for label_current, name_current in self.names.items():
+                if label_current != label and name_current == name_new:
+                    label_merge = label_current
+                    break
+
+            self.names[label] = change['new']
+            if label_merge is not None:
+                del self.names[label_merge]
+                self.colors[label] = self.colors[label_merge]
+                del self.colors[label_merge]
+                for i in range(len(self.labels)):
+                    if self.labels[i] == label_merge:
+                        self.labels[i] = label
+                self.refresh()
+
+        return _update
+
+    def _update_color(self, i: int, label: Hashable) -> None:
+        def _update(change: Dict):
+            self.colors[label] = change['new']
+            self.plot.marks[i].colors = [change['new']]
+        return _update
+
+    def _make_legend(self) -> wg.DOMWidget:
+        out = wg.Output()
+        display(out)
+
+        items_legend = []
+        for i, label in enumerate(uniques_sorted(self.labels)):
+            picker = wg.ColorPicker(
+                concise=True,
+                value=self.colors[label],
+                layout=wg.Layout(width="2em")
+            )
+            picker.observe(self._update_color(i, label), "value")
+            textbox = wg.Text(
+                value=self.names[label],
+                continuous_update=False,
+                layout=wg.Layout(width="10em")
+            )
+            textbox.observe(self._update_name(label), "value")
+            items_legend.append(
+                wg.HBox(
+                    children=[picker, textbox],
+                    layout=wg.Layout(min_height="2.2em")
+                )
+            )
+        return wg.VBox(children=items_legend, layout=wg.Layout(min_width="25%"))
 
     @contextmanager
     def editing(self) -> Iterable["Labeler"]:
         yield self
         self.refresh()
+
+    @property
+    def labels_named(self) -> Sequence[str]:
+        return [self.names[i] for i in self.labels]
 
 
 COLORS = """\
