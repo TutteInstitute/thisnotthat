801602669f8042f8e3fd76662d104e425300a04e
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2021-12-02 16:39:38 -0500

    Bump version to 0.1

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="thisnotthat",
-    packages=find_packages()
+    packages=find_packages(),
+    version="0.1",
+    install_requires=["bqplot"]
 )
