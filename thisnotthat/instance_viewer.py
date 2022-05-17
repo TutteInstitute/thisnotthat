4c5ae5a9f7e280f7f7011609c4b957270a22f421
Leland McInnes <leland.mcinnes@gmail.com>
2022-05-17 15:18:25 -0400

    Start an information panel for viewing instance details

---
+++ thisnotthat/instance_viewer.py
@@ -0,0 +1,38 @@
+import panel as pn
+import param
+import pandas as pd
+
+class InformationPane(pn.reactive.Reactive):
+
+    selected = param.List(default=[], doc="Indices of selected samples")
+    data = param.DataFrame(doc="Source data")
+
+    def __init__(
+            self,
+            raw_dataframe: pd.DataFrame,
+            markdown_template: str,
+            *,
+            width: int = 200,
+            height: int = 600,
+            placeholder_text: str = "<center> ... nothing selected ..."
+    )
+        self.data = raw_dataframe
+        self.markdown_template = markdown_template
+        self.placeholder_text = placeholder_text
+        self.pane = pn.pane.Markdown(self.placeholder_text, width=width, height=height)
+
+    def _get_model(self, *args, **kwds):
+        return self.pane._get_model(*args, **kwds)
+
+    @param.depends("selected", watch=True)
+    def _info_pane_update_selection(self) -> None:
+        if len(self.selected) == 0:
+            self.pane.object = self.placeholder_text
+        else:
+            substitution_dict = {
+                col: self.dataframe[col].iloc[self.selected[-1]]
+                for col in self.dataframe.columns
+            }
+            self.pane.object = self.markdown_template.format(
+                substitution_dict
+            )
