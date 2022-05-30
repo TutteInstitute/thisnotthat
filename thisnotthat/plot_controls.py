aa1d1b7b741cac456defa0f84400bf0f3db02117
Leland McInnes <leland.mcinnes@gmail.com>
2022-05-30 12:06:39 -0400

    Start fleshing out plot _controls

--- thisnotthat/plot_controls.py
+++ thisnotthat/plot_controls.py
@@ -7,6 +7,7 @@ import bokeh.palettes
 
 from typing import *
 
+
 class PlotControlPane(pn.reactive.Reactive):
 
     color_by_vector = param.Series(doc="Color by")
@@ -15,25 +16,69 @@ class PlotControlPane(pn.reactive.Reactive):
     hover_text = param.List([], item_type=str, doc="Hover text")
 
     def __init__(self, raw_dataframe: pd.DataFrame, *, name="Plot Controls"):
-        super().__init__()
+        super().__init__(name=name)
         self.dataframe = raw_dataframe
 
         self.palette_selector = pn.widgets.Select(
             name="Color Palette",
-            options=bokeh.palettes.__palettes__,
+            groups={
+                "Default": ["Default palette"],
+                "ColorBrewer palettes": list(bokeh.palettes.brewer.keys()),
+                "D3 palettes": list(bokeh.palettes.d3.keys()),
+                "Smooth palettes": [
+                    "Viridis",
+                    "Cividis",
+                    "Gray",
+                    "Inferno",
+                    "Magma",
+                    "Plasma",
+                    "Turbo",
+                ],
+            },
         )
-        self.column_selected = pn.widgets.Select(
-            name="Color by column",
-            options=list(self.dataframe.columns),
+        self.palette_selector.param.watch(self._palette_change, "value")
+        self.color_by_column = pn.widgets.Select(
+            name="Color by column", options=["Default"] + list(self.dataframe.columns),
         )
-        self.apply_button = pn.widgets.Button(
-            name="Apply", button_type="success"
+        self.color_by_column.param.watch(self._color_by_change, "value")
+        self.hover_text_column = pn.widgets.Select(
+            name="Hover text column",
+            options=["Default"] + list(self.dataframe.columns),
         )
+        self.hover_text_column.param.watch(self._hover_text_change, "value")
+        self.marker_size_column = pn.widgets.Select(
+            name="Marker size column",
+            options=["Default"]
+            + list(self.dataframe.select_dtypes(include="number").columns),
+        )
+        self.marker_size_column.param.watch(self._marker_size_change, "value")
+        # self.apply_button = pn.widgets.Button(
+        #     name="Apply", button_type="success"
+        # )
         self.pane = pn.WidgetBox(
             self.palette_selector,
-            self.column_selected,
-            self.apply_button,
+            self.color_by_column,
+            self.marker_size_column,
+            self.hover_text_column,
+            # self.apply_button,
         )
 
     def _get_model(self, *args, **kwds):
         return self.pane._get_model(*args, **kwds)
+
+    def _palette_change(self, event) -> None:
+        if pd.api.types.is_numeric_dtype(self.dataframe[self.color_by_column.value]):
+            # Continuous scale required
+            pass
+        else:
+            # Discrete scale required
+            pass
+
+    def _color_by_change(self, event) -> None:
+        pass
+
+    def _hover_text_change(self, event) -> None:
+        pass
+
+    def _marker_size_change(self, event) -> None:
+        pass
