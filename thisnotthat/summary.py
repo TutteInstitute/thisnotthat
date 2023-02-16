e72a43292a05f17b3ed8b7dcd86603371cc504c0
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2023-02-16 22:31:28 +0000

    Summary plot pane and feature importance summarizer

--- thisnotthat/summary.py
+++ thisnotthat/summary.py
@@ -1,8 +1,12 @@
 from typing import Callable, Optional, Protocol, Sequence
 
+from bokeh.layouts import LayoutDOM
+import bokeh.plotting as bpl
+import numpy as np
 import pandas as pd
 import panel as pn
 import param
+from sklearn.linear_model import LogisticRegression
 
 
 class DataFrameSummarizer(Protocol):
@@ -153,3 +157,182 @@ class SummarizerValueCounts:
         """
         data_selected = self.data.iloc[selected]
         return data_selected.value_counts().to_frame().head(self.top_k)
+
+
+class PlotSummarizer(Protocol):
+
+    def summarize(self, selected: Sequence[int]) -> LayoutDOM: ...
+
+
+def display_no_selection() -> LayoutDOM:
+    fig = bpl.figure()
+    fig.text([0], [0], ["Nothing to summarize."])
+    return fig
+
+
+PlotNoSelection = Callable[[], LayoutDOM]
+
+
+class PlotSummaryPane(pn.reactive.Reactive):
+    """
+    A data pane that displays a summary of the selected points in the form of a
+    plot. This display is regenerated every time the selection changes in the linked
+    plot.
+
+    summarizer
+        Any object with a ``summarize`` method that will take the list of (indices of)
+        selected points and produce (and return) a Bokeh displayable object that
+        summarizes visually the selection. This method will not be called when no point
+        is selected, so one may assume the list of indices not to be empty. The
+        ``thisnotthat.summary`` module contains a set of such summarizer classes.
+
+    no_selection
+        Parameter-less function that returns a Bokeh displayable object that should come
+        up when no point is selected.
+
+    width
+    height
+    sizing_mode
+        Geometry of the displayed figure. See Panel documentation.
+
+    name
+        Name of the pane.
+
+
+    Example: (in a Jupyter notebook)
+
+    from bokeh.plotting import figure
+    import numpy as np
+    import pandas as pd
+
+    data = pd.DataFrame({
+        "x": [8, 3, 9, -2, 4],
+        "y": [7, 0, 11, -5, -3]
+    })
+
+    class MeansSummarizer:
+
+        def summarize(self, selected):
+            features = list(data.columns)
+            fig = figure(y_range=features)
+            fig.hbar(y=features, right=np.squeeze(np.mean(data.iloc[selected])))
+            return fig
+
+    plot = tnt.BokehPlotPane(data, show_legend=False)
+    summary = tnt.SummaryDataPane(MeansSummarizer())
+    summary.link_to_plot(plot)
+    display(pn.Row(plot, summary))
+
+    Now play with the lasso tool to select points, and see the data pane show a bar
+    diagram of the mean features of the points.
+    """
+
+    selected = param.List(default=[], doc="Indices of selected samples")
+
+    def __init__(
+        self,
+        summarizer: PlotSummarizer,
+        no_selection: PlotNoSelection = display_no_selection,
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+        sizing_mode: str = "stretch_both",
+        name: str = "Summary"
+    ) -> None:
+        super().__init__(name=name)
+        self.summarizer = summarizer
+        self.no_selection = no_selection
+        self._base_selection = []
+        self._geometry_figure = {
+            name: param
+            for name, param in [("width", width), ("height", height)]
+            if param
+        }
+        self._geometry_pane = {
+            **self._geometry_figure,
+            **{
+                name: param
+                for name, param in [("sizing_mode", sizing_mode)]
+                if param
+            }
+        }
+        self.summary_plot = pn.pane.Bokeh(
+            self.no_selection(),
+            sizing_mode=sizing_mode,
+            width=width,
+            height=height
+        )
+        self.pane = pn.Column(self.summary_plot, sizing_mode=sizing_mode)
+
+    def _get_model(self, *args, **kwargs):
+        return self.pane._get_model(*args, **kwargs)
+
+    @param.depends("selected", watch=True)
+    def _update_selected(self) -> None:
+        # self.pane[0] = pn.pane.Bokeh(
+        #     fig,
+        #     **self._geometry_pane
+        # )
+        self.summary_plot.object = (
+            self.summarizer.summarize(self.selected)
+            if self.selected
+            else self.no_selection()
+        )
+
+    def link_to_plot(self, plot):
+        """
+        Link this pane to the plot pane to summarize using a default set of params
+        that can sensibly be linked.
+
+        Parameters
+        ----------
+        plot: PlotPane
+            The plot pane to link to.
+
+        Returns
+        -------
+        link:
+            The link object.
+        """
+        return self.link(plot, selected="selected", bidirectional=True)
+
+
+class FeatureImportanceSummarizer:
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        max_features: int = 15,
+        tol_importance_relative: float = 0.01
+    ) -> None:
+        self.data = data.select_dtypes(np.number)  # Indexed 0 to length.
+        self.max_features = max_features
+        self.tol_importance_relative = tol_importance_relative
+
+    def summarize(self, selected: Sequence[int]) -> LayoutDOM:
+        classes = np.zeros((len(self.data),), dtype="int32")
+        classes[selected] = 1
+        classifier = LogisticRegression(
+            penalty="l1",
+            solver="liblinear",
+            class_weight="balanced"
+        ).fit(
+            self.data.to_numpy(),
+            classes
+        )
+        assert classifier.coef_.shape[0] == 1 or classifier.coef_.ndim == 1
+        importance = np.squeeze(classifier.coef_)
+        index_importance = np.argsort(-np.abs(importance))[:self.max_features]
+        importance_abs = np.abs(importance)[index_importance]
+        importance_relative = importance_abs / np.max(importance_abs)
+        importance_restricted = importance[
+            np.where(importance_relative > self.tol_importance_relative)
+        ]
+
+        x = list(self.data.columns[index_importance[:len(importance_restricted)]])
+        fig = bpl.figure(x_range=x)
+        fig.vbar(
+            x=x,
+            top=importance[index_importance[:len(importance_restricted)]],
+            width=0.8
+        )
+        return fig
